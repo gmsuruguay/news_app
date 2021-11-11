854e6111204158bd7c278e41160e33ecd792ce10
martin_dev <gmsuruguay@gmail.com>
2021-11-11 15:22:49 -0300

    se creo migracion model y controller para la entidad favorite

---
+++ database/migrations/2021_11_11_175858_create_favorites_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('url');
+            $table->integer('id_new');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');  
+            $table->tinyInteger('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
